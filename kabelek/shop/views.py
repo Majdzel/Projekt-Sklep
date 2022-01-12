a3a1d2f492a620f6036ba395f987686bffa8468c
Majdzel <92318639+Majdzel@users.noreply.github.com>
2022-01-12 23:06:44 +0100

    dodanie wyswietalnie items

--- kabelek/shop/views.py
+++ kabelek/shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import items
 
+def itemsList(request):
+
+    allItems = items.objects.all()
+
+
+    context = {'allItems': allItems}
+
+    return render(request, 'shop/itemsList.html', context)
 # Create your views here.
